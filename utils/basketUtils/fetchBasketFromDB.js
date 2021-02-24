e8674c5c32c771fb05830f668bfd0d3c8213525e
Lee Bates <batespcm1981@gmail.com>
2021-02-24 07:23:52 +0000

    functionality to search a mock nosql db

--- utils/basketUtils/fetchBasketFromDB.js
+++ utils/basketUtils/fetchBasketFromDB.js
@@ -1,5 +1,15 @@
-const fetchBasketFromDB = (sessionID) => {
+const fakeSessionsDB = [{ sessionID: 1, item: { productID: 10, productTitle: "Item 1", price: 11, weight: 1, deliveryDays: 4 } }, { sessionID: 2, item: { productID: 20, productTitle: "Item 2", price: 22, weight: 8, deliveryDays: 1 } }]
 
+const fetchBasketFromDB = (sessID, itemAdded) => {
+    let liveBasket = itemAdded
+    const userBasket = fakeSessionsDB.filter(basket => {
+        return basket.sessionID === sessID
+    })
+    if (userBasket.length === 1) {
+        liveBasket = userBasket[0].item
+    }
+    return liveBasket
 }
+
 
 module.exports = fetchBasketFromDB
